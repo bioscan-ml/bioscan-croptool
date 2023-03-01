44744fd07f582cf3f18dc0a5a0ce1bb059c50a6a
zmgong <ming2280089874@gmail.com>
2023-02-28 21:15:44 -0800

    Refactor the code and delete unnecessary package from util.

--- scripts/train.py
+++ scripts/train.py
@@ -87,30 +87,6 @@ def prepare_for_coco_detection(predictions):
         )
     return coco_results
 
-def prepare_for_coco_detection(predictions):
-    coco_results = []
-    for original_id, prediction in predictions.items():
-        if len(prediction) == 0:
-            continue
-
-        boxes = prediction["boxes"]
-        boxes = convert_to_xywh(boxes).tolist()
-        scores = prediction["scores"].tolist()
-        labels = prediction["labels"].tolist()
-
-        coco_results.extend(
-            [
-                {
-                    "image_id": original_id,
-                    "category_id": labels[k],
-                    "bbox": box,
-                    "score": scores[k],
-                }
-                for k, box in enumerate(boxes)
-            ]
-        )
-    return coco_results
-
 def evaluation(model, val_dataset, val_dataloader, feature_extractor):
 
     iou_types = ['bbox']
