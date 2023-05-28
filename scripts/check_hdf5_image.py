6739af0b46d4603627c977f7537eda2720618d32
zmgong <ming2280089874@gmail.com>
2023-05-28 09:57:11 -0700

    Using BytesIO to read jpg file directly.

--- scripts/check_hdf5_image.py
+++ scripts/check_hdf5_image.py
@@ -7,7 +7,11 @@ from PIL import Image
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    with h5py.File("hdf5_workspace/output_hdf5/cropped_25.hdf5", 'r') as hdf5_file:
+    with h5py.File("hdf5_workspace/output_hdf5/cropped_256.hdf5", 'r') as hdf5_file:
         images = hdf5_file['bioscan_dataset']
-        img = Image.open(io.BytesIO(np.array(images[images.keys[0]])))
-        img.show()
+        first_key = [key for key in images.keys()][0]
+        if "ABCKHUA" in images.keys():
+            print("File exist")
+        byte_io = io.BytesIO(np.array(images[first_key]))
+        image = Image.open(byte_io)
+        image.show()
--- scripts/save_images_to_hdf5.py
+++ scripts/save_images_to_hdf5.py
@@ -5,17 +5,14 @@ import io
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+
 """
 This is a special one time script for special purpose,  will be removed from the repo after the task is done.
 """
 
-def save_single_image(image, image_name, hdf5):
+def save_single_image(binary_data_np, image_name, hdf5):
     dataset = hdf5['bioscan_dataset']
-    binary_data_io = io.BytesIO()
-    image.save(binary_data_io, format='JPEG')
     # Read the binary data from the BytesIO object
-    binary_data = binary_data_io.getvalue()
-    binary_data_np = np.frombuffer(binary_data, dtype=np.uint8)
     dataset.create_dataset(image_name, data=binary_data_np)
 
 
@@ -34,11 +31,11 @@ if __name__ == '__main__':
                         help="Folder that contains the original images.")
     parser.add_argument('--hdf5_path', type=str, default="hdf5_workspace/output_hdf5/cropped_256.hdf5",
                         help="Path to the output hdf5 file.")
-    parser.add_argument('--datatype', type=str, default="cropped",
+    parser.add_argument('--image_set', type=str, default="cropped_256",
                         help="Original or cropped images")
     args = parser.parse_args()
 
-    make_hdf5(path=args.hdf5_path, data_typ=args.datatype)
+    make_hdf5(path=args.hdf5_path, data_typ=args.image_set)
     pbar_parts = tqdm(os.listdir(args.input_dir))
     with h5py.File(args.hdf5_path, 'a') as hdf5:
         for sub_dir in pbar_parts:
@@ -46,7 +43,15 @@ if __name__ == '__main__':
             path_to_part_folder = os.path.join(args.input_dir, sub_dir)
             pbar_files = tqdm(os.listdir(path_to_part_folder))
             for file_name in pbar_files:
+                if file_name in hdf5['bioscan_dataset'].keys():
+                    with open(args.image_set + '_hdf5_writing_log.txt', 'w') as f:
+                        f.write(file_name + "already exist.")
+                    continue
                 pbar_files.set_description("Current part progress")
                 file_path = os.path.join(path_to_part_folder, file_name)
-                image = Image.open(file_path)
-                save_single_image(image, file_name, hdf5)
+                with open(file_path, 'rb') as f:
+                    image_data = f.read()
+                byte_io = io.BytesIO(image_data)
+                binary_data = byte_io.getvalue()
+                binary_data_np = np.frombuffer(binary_data, dtype=np.uint8)
+                save_single_image(binary_data_np, file_name, hdf5)
