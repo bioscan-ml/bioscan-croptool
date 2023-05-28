414442d90dbf5bc54e290cd7c67e54265fc87b61
zmgong <ming2280089874@gmail.com>
2023-05-28 08:18:46 -0700

    add a script for generating hdf5 files.

---
+++ scripts/for_resize_images.py
@@ -0,0 +1,244 @@
+import argparse
+import os
+import sys
+from os.path import dirname, abspath
+import numpy as np
+import torch
+from tqdm import tqdm
+from transformers import DetrFeatureExtractor
+from PIL import Image, ImageDraw, ImageOps
+import shutil
+
+project_dir = dirname(dirname(abspath(__file__)))
+sys.path.append(project_dir)
+from model.detr import load_model_from_ckpt
+from util.visualize_and_process_bbox import get_bbox_from_output, scale_bbox
+
+"""
+This is a special one time script for special purpose,  will be removed from the repo after the task is done.
+"""
+
+
+def unzip_tars_to_folder(path_to_tar, path_to_unzipped_folder):
+    # open file
+    os.system('tar -xvf ' + path_to_tar + ' --strip=7 -C ' + path_to_unzipped_folder)
+
+    file.close()
+
+
+def get_size_with_aspect_ratio(image_size, size):
+    # Reference:
+    # https://huggingface.co/transformers/v4.9.2/_modules/transformers/models/detr/feature_extraction_detr.html
+    w, h = image_size
+
+    if (w <= h and w == size) or (h <= w and h == size):
+        return w, h
+
+    if w < h:
+        ow = size
+        oh = int(size * h / w)
+    else:
+        oh = size
+        ow = int(size * w / h)
+
+    return ow, oh
+
+
+def expand_image(args, image, size, direction):
+    border_color = (args.background_color_R, args.background_color_G, args.background_color_B)
+    border_image = None
+    if direction == 'left':
+        border_image = ImageOps.expand(image, border=(size, 0, 0, 0), fill=border_color)
+    elif direction == 'top':
+        border_image = ImageOps.expand(image, border=(0, size, 0, 0), fill=border_color)
+    elif direction == 'right':
+        border_image = ImageOps.expand(image, border=(0, 0, size, 0), fill=border_color)
+    elif direction == 'bottom':
+        border_image = ImageOps.expand(image, border=(0, 0, 0, size), fill=border_color)
+    else:
+        exit("Wrong expand direction.")
+
+    return border_image
+
+
+def rotate_image_and_bbox_if_necesscary(image, left, top, right, bottom):
+    image_size = image.size
+    image = image.rotate(90, expand=True)
+    new_left = top
+    new_right = bottom
+    new_bottom = image_size[0] - left
+    new_top = image_size[0] - right
+
+    return image, new_left, new_top, new_right, new_bottom
+
+
+def change_size_to_4_3(left, top, right, bottom):
+    width = right - left
+    height = bottom - top
+    if width < height / 3 * 4:
+        extend_length = height / 3 * 4 - width
+        left = left - extend_length / 2
+        right = right + extend_length - extend_length / 2
+
+    elif height < width / 4 * 3:
+        extend_length = width / 4 * 3 - height
+        top = top - extend_length / 2
+        bottom = bottom + extend_length - extend_length / 2
+    return left, top, right, bottom
+
+
+def crop_image(args, model, feature_extractor, device):
+    """
+    Crop and save images based on the predicted bounding boxes from the model.
+    :param model: Detr model that loaded from the checkpoint.
+    :param feature_extractor: A ResNet50 model as a standard image extractor.
+    """
+
+    list_of_un_cropped_images = []
+    path_to_cropped_folder = os.path.join(args.local_output_dir, "cropped_" + args.current_image_folder_name)
+    path_to_cropped_and_resized_folder = os.path.join(args.local_output_dir,
+                                                      "cropped_resized_" + args.current_image_folder_name)
+
+    os.makedirs(path_to_cropped_folder, exist_ok=True)
+    os.makedirs(path_to_cropped_and_resized_folder, exist_ok=True)
+    list_of_cropped_images = os.listdir(path_to_cropped_folder)
+    list_of_cropped_and_resized_images = os.listdir(path_to_cropped_and_resized_folder)
+
+    pbar_in_crop_image = tqdm(os.listdir(args.input_dir))
+    for filename in pbar_in_crop_image:
+        pbar_in_crop_image.set_description("Checking the un-cropped images.")
+        name_of_cropped_image = "cropped_" + filename
+        name_of_cropped_and_resized_image = "cropped_resized_" + filename
+        if name_of_cropped_image not in list_of_cropped_images or name_of_cropped_and_resized_image not in list_of_cropped_and_resized_images:
+            list_of_un_cropped_images.append(filename)
+    pbar_in_crop_image = tqdm(list_of_un_cropped_images)
+    for filename in pbar_in_crop_image:
+        pbar_in_crop_image.set_description("Cropping images.")
+        f = os.path.join(args.input_dir, filename)
+        if os.path.isfile(f):
+            try:
+                image = Image.open(f)
+            except:
+                print("Image not found in: " + f)
+                exit(1)
+            encoding = feature_extractor(images=image, return_tensors="pt").to(device)
+            pixel_values = encoding["pixel_values"].squeeze().unsqueeze(0)
+            outputs = model(pixel_values=pixel_values, pixel_mask=None)
+            pred_logit = outputs.logits[0]
+            pred_boxes = outputs.pred_boxes[0]
+            bbox = get_bbox_from_output(pred_logit, pred_boxes, image).detach().numpy()
+            bbox = np.round(bbox, 0)
+            left, top, right, bottom = bbox[0], bbox[1], bbox[2], bbox[3]
+            if args.show_bbox:
+                draw = ImageDraw.Draw(image)
+                draw.rectangle((left, top, right, bottom), outline=(255, 0, 0), width=args.width_of_bbox)
+            left, top, right, bottom = scale_bbox(args, left, top, right, bottom)
+
+            image_size = image.size
+
+            if args.fix_ratio:
+                width = right - left
+                height = bottom - top
+
+                if height > width and args.rotate_image:
+                    image, left, top, right, bottom = rotate_image_and_bbox_if_necesscary(image, left, top, right,
+                                                                                          bottom)
+                    image_size = image.size
+
+                left, top, right, bottom = change_size_to_4_3(left, top, right, bottom)
+                left = round(left)
+                top = round(top)
+                right = round(right)
+                bottom = round(bottom)
+                # if left < 0 or top < 0 or right > image_size[0] or bottom > image_size[1]:
+                #     print("Please crop this image manually: " + f)
+
+            # cropped_img = image.crop((max(left, 0), max(top, 0), min(right, image_size[0]), min(bottom, image_size[1])))
+
+            # Check if width is smaller than the bbox
+
+            if left < 0:
+                border_size = 0 - left
+                right = right - left
+                left = 0
+                image = expand_image(args, image, border_size, 'left')
+
+            if top < 0:
+                border_size = 0 - top
+                bottom = bottom - top
+                top = 0
+                image = expand_image(args, image, border_size, 'top')
+
+            if right > image.size[0]:
+                border_size = right - image.size[0] + 1
+                image = expand_image(args, image, border_size, 'right')
+
+            if bottom > image.size[1]:
+                border_size = bottom - image.size[1] + 1
+                image = expand_image(args, image, border_size, 'bottom')
+
+            cropped_img = image.crop((left, top, right, bottom))
+
+            cropped_img.save(os.path.join(path_to_cropped_folder, "cropped_" + filename))
+            if args.save_resized:
+                new_width, new_height = get_size_with_aspect_ratio(cropped_img.size, 256)
+                cropped_and_resized_img = cropped_img.resize((new_width, new_height))
+                cropped_and_resized_img.save(os.path.join(path_to_cropped_and_resized_folder,
+                                                          "cropped_resized_" + filename))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--input_dir', type=str, required=True,
+    #                     help="Folder that contains the original images.")
+    parser.add_argument('--input_txt', type=str, required=True,
+                        help="Path to the txt file that contains the names of tar files you want to download to local."
+                             "storage.")
+    parser.add_argument('--remote_input_dir', type=str, required=True,
+                        help="Path to the directory that contains the image folders you want to download to your local "
+                             "storage.")
+    parser.add_argument('--local_input_dir', type=str, default="local_input_dir",
+                        help="Path to the directory that you want to save the un-cropped image directories and the "
+                             "checkpoint")
+    parser.add_argument('--local_output_dir', type=str, default="local_output_dir",
+                        help="Folder that will contain the cropped images in both un-resized and resized.")
+    parser.add_argument('--remote_output_dir', type=str, default="cropped_image",
+                        help="Folder that will contain the cropped images in both un-resized and resized.")
+
+    args = parser.parse_args()
+    os.makedirs(args.local_input_dir, exist_ok=True)
+    os.makedirs(args.local_output_dir, exist_ok=True)
+
+    with open(args.input_txt) as file:
+        image_tar_names = [line.rstrip() for line in file]
+
+    pbar = tqdm(image_tar_names)
+    for tarfile_name in pbar:
+        pbar.set_description("Copying, unzipping, cropping")
+        folder_name = tarfile_name.replace(".tar", "")
+        target_folder_path = os.path.join(args.local_input_dir, folder_name)
+        src_tar_path = os.path.join(args.remote_input_dir, tarfile_name)
+        target_tar_path = os.path.join(args.local_input_dir, tarfile_name)
+
+        # Unzip tar file to the target folder. Remove the tar file.
+        if not os.path.exists(target_folder_path):
+            os.makedirs(target_folder_path, exist_ok=True)
+            if not os.path.exists(target_tar_path):
+                shutil.copyfile(src_tar_path, target_tar_path)
+                unzip_tars_to_folder(target_tar_path, target_folder_path)
+                os.remove(target_tar_path)
+
+        input_folder_dir = os.path.join(args.local_input_dir, folder_name)
+        resize_folder_dir = os.path.join(args.local_output_dir, folder_name)
+        os.makedirs(resize_folder_dir, exist_ok=True)
+
+        os.system("find " + input_folder_dir + " -name '*.jpg' -exec bash -c 'convert ${0} -resize x256 " + resize_folder_dir + "-256/${0##*/}' {} \;")
+
+        shutil.rmtree(os.path.join(args.local_input_dir, folder_name))
+
+        if os.path.exists(os.path.join(args.remote_output_dir, folder_name)):
+            shutil.rmtree(os.path.join(args.remote_output_dir, folder_name))
+        shutil.copytree(os.path.join(args.local_output_dir, "cropped_" + folder_name),
+                        os.path.join(args.remote_output_dir, "cropped_" + folder_name))
+        shutil.copytree(os.path.join(args.local_output_dir, "cropped_resized_" + folder_name),
+                        os.path.join(args.remote_output_dir, "cropped_resized_" + folder_name))
---
+++ scripts/save_images_to_hdf5.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import h5py
+import io
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+"""
+This is a special one time script for special purpose,  will be removed from the repo after the task is done.
+"""
+
+def save_single_image(image, image_name, hdf5):
+    dataset = hdf5['bioscan_dataset']
+    binary_data_io = io.BytesIO()
+    image.save(binary_data_io, format='JPEG')
+    # Read the binary data from the BytesIO object
+    binary_data = binary_data_io.getvalue()
+    binary_data_np = np.frombuffer(binary_data, dtype=np.uint8)
+    dataset.create_dataset(f"CROPPED_{image_name}", data=binary_data_np)
+
+
+def make_hdf5(path='', data_typ=''):
+    with h5py.File(path, 'w') as hdf5:
+        dataset = hdf5.create_group('bioscan_dataset')
+        dataset.attrs['Description'] = f'BioScan Dataset: {data_typ} Images'
+        dataset.attrs['Copyright Holder'] = 'CBG Photography Group'
+        dataset.attrs['Copyright Institution'] = 'Centre for Biodiversity Genomics (email:CBGImaging@gmail.com)'
+        dataset.attrs['Photographer'] = 'CBG Robotic Imager'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default="hdf5_workspace/unzipped/cropped_256",
+                        help="Folder that contains the original images.")
+    parser.add_argument('--hdf5_path', type=str, default="hdf5_workspace/output_hdf5/cropped_256.hdf5",
+                        help="Path to the output hdf5 file.")
+    parser.add_argument('--datatype', type=str, default="cropped",
+                        help="Original or cropped images")
+    args = parser.parse_args()
+
+    make_hdf5(path=args.hdf5_path, data_typ=args.datatype)
+    pbar_parts = tqdm(os.listdir(args.input_dir))
+    with h5py.File(args.hdf5_path, 'a') as hdf5:
+        for sub_dir in pbar_parts:
+            pbar_parts.set_description("Parts")
+            path_to_part_folder = os.path.join(args.input_dir, sub_dir)
+            pbar_files = tqdm(os.listdir(path_to_part_folder))
+            for file_name in pbar_files:
+                pbar_files.set_description("Current part progress")
+                file_path = os.path.join(path_to_part_folder, file_name)
+                image = Image.open(file_path)
+                save_single_image(image, file_name, hdf5)
