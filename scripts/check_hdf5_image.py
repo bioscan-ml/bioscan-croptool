b42bb77a6afedb809ad97d01fa0c09b945105965
zmgong <ming2280089874@gmail.com>
2023-05-28 14:41:54 -0700

    Update the script that is using to check the hdf5 file.

--- scripts/check_hdf5_image.py
+++ scripts/check_hdf5_image.py
@@ -10,8 +10,6 @@ if __name__ == '__main__':
     with h5py.File("hdf5_workspace/output_hdf5/cropped_256.hdf5", 'r') as hdf5_file:
         images = hdf5_file['bioscan_dataset']
         first_key = [key for key in images.keys()][0]
-        if "ABCKHUA" in images.keys():
-            print("File exist")
         byte_io = io.BytesIO(np.array(images[first_key]))
         image = Image.open(byte_io)
         image.show()
